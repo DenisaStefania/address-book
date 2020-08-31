abfd493c4928acac1cc284a9e089f407106e613a
Denisa Stefania Lazar <denisa-stefania.lazar@frequentis.com>
2020-08-31 13:54:33 +0300

    update after feedback

--- src/main/java/address/book/AddressBook.java
+++ src/main/java/address/book/AddressBook.java
@@ -2,40 +2,10 @@ package address.book;
 
 import contacts.Contact;
 
-import java.util.List;
+public interface AddressBook {
+    void add(Contact contact);
 
-public class AddressBook implements IAddressBook {
-    private List<Contact> contacts;
+    boolean delete(Contact contact);
 
-    public AddressBook(List<Contact> contacts) {
-        this.contacts = contacts;
-    }
-
-    public List<Contact> getContacts() {
-        return contacts;
-    }
-
-    public void add(Contact contact) {
-        if(!contacts.contains(contact)) {
-            contacts.add(contact);
-        }
-    }
-
-    public boolean delete(Contact contact) {
-        if(contacts.contains(contact)) {
-            contacts.remove(contact);
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    public Contact find(final String contactName) {
-        for (Contact c : contacts) {
-            if (c.getName().equalsIgnoreCase(contactName)) {
-                return c;
-            }
-        }
-        return null;
-    }
+    Contact find(String contactName);
 }
